314d0e8877e8c78fccd314125bbc43b8c5ea4f11
archifix <107472693+archifix@users.noreply.github.com>
2022-09-15 18:28:39 +0300

    15.09

--- src/components/Layuot/Navigation.tsx
+++ src/components/Layuot/Navigation.tsx
@@ -10,36 +10,38 @@ import "../styles/navigation.scss"
 
 export function Navigation() {
   return (
-    <nav className="flex justify-between items-center h-[56px] max-w-[1200px] ml-auto mr-auto px-5 shadow-lg text-sm uppercase">
-      <div className="flex items-center">
-        <img src={logo} className="mr-[12px]" alt="" />
-        <img src={line} className="mr-[12px]" alt="" />
-        <img src={logo2} alt="" />
-      </div>
-      <div className="nav-title flex justify-between items-center text-center font-med">
-        <Link to="/" className="mr-5">
-          Каталог
-        </Link>
-        <Link to="/Contacts" className="ml-5">
-          Контакты
-        </Link>
-        <Link to="/Contacts" className="ml-32">
-          Зарабатывай с нами
-        </Link>
-      </div>
-      <div className="font-med  text-sm">
-        <button className="w-4 h-4 pr-[24px]">EN</button>
-        <button className="pr-[24px] w-4 h-4">DE</button>
-        <button className="pr-[24px] w-4 h-4">FR</button>
-        <button className="pr-[24px] w-4 h-4">ES</button>
-        <button>RU</button>
-      </div>
-      <div className="flex justify-end items-center font-med">
-        <img src={share} alt="Поделится" />
-        <button className="uppercase">Поделится</button>
-        <img src={log_out} className="ml-10" alt="Поделится" />
-        <button className="uppercase">Войти</button>
-      </div>
-    </nav>
+    <header className="border-b-2 shadow-lg">
+      <nav className="flex justify-between items-center h-[56px] max-w-[1200px] ml-auto mr-auto px-5  text-sm uppercase">
+        <div className="flex items-center">
+          <img src={logo} className="mr-[12px]" alt="" />
+          <img src={line} className="mr-[12px]" alt="" />
+          <img src={logo2} alt="" />
+        </div>
+        <div className="nav-title flex justify-between items-center text-center font-med">
+          <Link to="/" className="mr-5">
+            Каталог
+          </Link>
+          <Link to="/Contacts" className="ml-5">
+            Контакты
+          </Link>
+          <Link to="/Contacts" className="ml-32">
+            Зарабатывай с нами
+          </Link>
+        </div>
+        <div className="font-med  text-sm">
+          <button className="w-4 h-4 pr-[24px]">EN</button>
+          <button className="pr-[24px] w-4 h-4">DE</button>
+          <button className="pr-[24px] w-4 h-4">FR</button>
+          <button className="pr-[24px] w-4 h-4">ES</button>
+          <button>RU</button>
+        </div>
+        <div className="flex justify-end items-center font-med">
+          <img src={share} alt="Поделится" />
+          <button className="uppercase">Поделится</button>
+          <img src={log_out} className="ml-10" alt="Поделится" />
+          <button className="uppercase">Войти</button>
+        </div>
+      </nav>
+    </header>
   )
 }
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,29 +1,35 @@
 import React from "react"
 import Layout from "../Layuot/Layout"
-//import card from "../../assets/img/main/fon.jpg"
+import card from "../../assets/img/main/fon.png"
 import "../styles/home.scss"
 
 export function Home() {
   return (
     <>
       <Layout>
-        <main className="flex justify-between items-center pt-36">
-          <div className="title flex-row">
-            <h3 className="text-sm font-med uppercase">Кто мы такие?</h3>
+        <main className="pt-36">
+          <div className="title">
+            <h3 className="text-sm  font-med uppercase">Кто мы такие?</h3>
             <h1 className="text-6xl font-med uppercase">MIratex House</h1>
-            <div className="subtitle pl-[86px] text-sm font-med">
-              <p>
-                Мы перерабатываем грузовые контейнеры в автономные крипто-дома,
-                гостиницы, офисы, кафе, которые можно разместить там, где Вы
-                хотите. Они абсолютно не зависят от коммуникаций, полностью
-                готовы к эксплуатации, а в свободное от работы время они
-                добывают крипто-валюту.
-              </p>
-            </div>
+            <p className="inline-flex justify-end text-sm font-med">
+              Мы перерабатываем грузовые контейнеры в автономные крипто-дома,
+              гостиницы, офисы, кафе, которые можно разместить там, где Вы
+              хотите. Они абсолютно не зависят от коммуникаций, полностью готовы
+              к эксплуатации, а в свободное от работы время они добывают
+              крипто-валюту.
+            </p>
           </div>
-
+          {/* <div className="subtitle inline-flex justify-end pl-[86px] text-sm font-med">
+            <p>
+              Мы перерабатываем грузовые контейнеры в автономные крипто-дома,
+              гостиницы, офисы, кафе, которые можно разместить там, где Вы
+              хотите. Они абсолютно не зависят от коммуникаций, полностью готовы
+              к эксплуатации, а в свободное от работы время они добывают
+              крипто-валюту.
+            </p>
+          </div> */}
           <div className="pt-[150px]">
-            <img src="../../assets/img/main/fon.png" alt="" />
+            <img src={card} alt="" />
           </div>
         </main>
       </Layout>
